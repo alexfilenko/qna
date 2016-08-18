7399084201a6d0291d3b4a867ada3b7239056347
alexfilenko <filenkoav@gmail.com>
2016-08-18 14:14:13 +0300

    device config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :questions do
     resources :answers, shallow: true
+
+    root to: "question#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
