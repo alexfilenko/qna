1945a40bbb48aa8370942b94f5e204d2e6653c70
alexfilenko <filenkoav@gmail.com>
2016-08-18 19:54:00 +0300

    create_question_spec

---
+++ spec/acceptance/create_question_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'Question stories', %q{
+  In order to working with questions
+  As an authenticated user
+  I want to be able working with questions
+} do
+
+  scenario 'Authenticated user creates question' do
+    User.create!(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text'
+    click_on 'Create'
+
+    expect(page).to have_content 'Your question successfully created.'
+  end
+
+  scenario 'Non-authenticated user tries to create question' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
