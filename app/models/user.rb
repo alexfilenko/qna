b06a1fc88c6de60ffe20bc400c43a408066206ba
alexfilenko <filenkoav@gmail.com>
2016-08-23 09:32:40 +0300

    Добавил методы в модель

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :questions
+  has_many :answers
+
+  def autor_of?(resourse)
+  	resourse.user_id == self.id
+  end
 end
