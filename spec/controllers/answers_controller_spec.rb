c8f51feea39a24df06baebb01acb5d55f7f1aad6
alexfilenko <filenkoav@gmail.com>
2016-08-30 18:18:18 +0300

    25red

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -8,14 +8,15 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'GET #new' do
     sign_in_user
-    before {get :new, question_id: question }
-    
-      it 'assigns a new Answer to @answer' do
+
+    before { get :new, question_id: question }
+
+    it 'assigns a new Answer to @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
     end
-    
+
     it 'renders new view' do
-    expect(response).to render_template :new
+      expect(response).to render_template :new
     end
   end
 
@@ -36,26 +37,35 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     sign_in_user
-    context 'with valid attributes' do
 
-      it 'saves the new answer in the database'do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
+    context 'with valid attributes' do
+      it 'saves the new answer in th database' do
+        expect do
+          post :create,
+               question_id: question,
+               answer: attributes_for(:answer)
+        end.to change { question.answers.count }.by(1)
       end
 
-      it 'redirects to question path' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+      it 'redirect to question show view' do
+        post :create, question_id: question, answer: attributes_for(:answer)
         expect(response).to redirect_to question_path(assigns(:question))
       end
     end
 
     context 'with invalid attributes' do
-
       it 'does not save the answer' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) } }.to_not change(Answer, :count)
+        expect do
+          post :create,
+               question_id: question,
+               answer: attributes_for(:invalid_answer)
+        end.to_not change(Answer, :count)
       end
 
-      it 're-renders new view'do
-        post :create, params: { question_id: question, answer: attributes_for(:invalid_answer) }
+      it 're-render new view' do
+        post :create,
+             question_id: question,
+             answer: attributes_for(:invalid_answer)
         expect(response).to render_template :new
       end
     end
@@ -66,22 +76,29 @@ RSpec.describe AnswersController, type: :controller do
       log_in user
     end
 
-  context 'with valid attributes' do
-    it 'changes answer attributes' do
-      patch :update, params: {id: user_answer, question_id: question, answer: { body: 'text text' } }                
-      user_answer.reload
-      expect(user_answer.body).to eq 'new body'
+    context 'with valid attributes' do
+      it 'changes answer attributes' do
+        patch :update, id: user_answer,
+                       question_id: question,
+                       answer: { body: 'new body' }
+        user_answer.reload
+        expect(user_answer.body).to eq 'new body'
+      end
+
+      it 'redirects to the question' do
+        patch :update, id: user_answer,
+                       question_id: question,
+                       answer: { body: 'new body' }
+        expect(response).to redirect_to question
+      end
     end
 
-  it 'redirects to the question' do
-      patch :update, params: {id: user_answer, question_id: question, answer: { body: 'new body' } } 
-      expect(response).to redirect_to question
-    end
-  end
-
-  context 'with invalid attributes' do
+    context 'with invalid attributes' do
       before do
-        patch :update, params: {id: answer, question_id: question, answer: { body: nil } }        
+        patch :update,
+              id: answer,
+              question_id: question,
+              answer: { body: nil }
       end
 
       it 'does not change answer attributes' do
@@ -106,9 +123,9 @@ RSpec.describe AnswersController, type: :controller do
         .to change(Answer, :count).by(-1)
     end
 
-      it 'redirects to question' do
-        delete :destroy, id: user_answer, question_id: question
-        expect(response).to redirect_to question
-      end
+    it 'redirects to question' do
+      delete :destroy, id: user_answer, question_id: question
+      expect(response).to redirect_to question
     end
   end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  it {should validate_presence_of :email}
-  it {should validate_presence_of :password}
-  it {should have_many :questions}
-  it {should have_many :answers}
+RSpec.describe User do
+  describe 'validations tests' do
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+  end
 
-  describe '.author_of?(resource)' do
-    let(:user) { create(:user) }
-    let(:answer) { create(:answer) }
-
-    it 'returns true if resource.user_id == self.id' do
-      answer.user = user
-      expect(user.author_of?(answer)).to be true
-    end
-
-    it 'returns false if resource.user_id != self.id' do
-      expect(user.author_of?(answer)).to be false
-    end
+  describe 'associations tests' do
+    it { should have_many(:questions).dependent(:destroy) }
+    it { should have_many(:answers).dependent(:destroy) }
   end
 end
